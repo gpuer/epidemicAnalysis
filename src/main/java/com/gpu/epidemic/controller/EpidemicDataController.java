62393e5b8c3fefd0b6664d69aa7c941b6748002d
wuydit <wuydit@163.com>
2020-01-30 20:51:16 +0800

    添加接口

--- src/main/java/com/gpu/epidemic/controller/EpidemicDataController.java
+++ src/main/java/com/gpu/epidemic/controller/EpidemicDataController.java
@@ -47,7 +47,7 @@ public class EpidemicDataController {
      * @return 当天的情况
      */
     @ApiOperation("根据城市和时间获取数据")
-    @GetMapping("epidemicBy/{city}/{date}")
+    @GetMapping("epidemicBy/city/{city}/date/{date}")
     public ResponseEntity getEpidemicByCityAndDate(@PathVariable("city")String city,
                                                    @PathVariable("date") LocalDate date){
         return ResponseEntity.ok(epidemicDataService.getOne(
@@ -55,6 +55,51 @@ public class EpidemicDataController {
                         .eq(EpidemicDataEntity::getCity, city)
                         .eq(EpidemicDataEntity::getCreatedAt,date)
         ));
+    }
+
+    /**
+     * 根据城市和时间获取数据
+     * @param city 城市
+     * @return 当天的情况
+     */
+    @ApiOperation("根据城市和时间获取数据")
+    @GetMapping("epidemicBy/city/{city}")
+    public ResponseEntity getEpidemicByCity(@PathVariable("city")String city){
+        return ResponseEntity.ok(epidemicDataService.list(
+                Wrappers.<EpidemicDataEntity>lambdaQuery()
+                        .eq(EpidemicDataEntity::getCity, city))
+        );
+    }
+
+    /**
+     * 根据省份和时间获取数据
+     * @param area 城市
+     * @param date 时间
+     * @return 当天的情况
+     */
+    @ApiOperation("根据城市和时间获取数据")
+    @GetMapping("epidemicBy/area/{area}/date/{date}")
+    public ResponseEntity getEpidemicByaAreaAndDate(@PathVariable("area")String area,
+                                                   @PathVariable("date") LocalDate date){
+        return ResponseEntity.ok(epidemicDataService.getOne(
+                Wrappers.<EpidemicDataEntity>lambdaQuery()
+                        .eq(EpidemicDataEntity::getArea, area)
+                        .eq(EpidemicDataEntity::getCreatedAt,date)
+        ));
+    }
+
+    /**
+     * 根据省份和时间获取数据
+     * @param area 省份
+     * @return 当天的情况
+     */
+    @ApiOperation("根据城市和时间获取数据")
+    @GetMapping("epidemicBy/area/{area}")
+    public ResponseEntity getEpidemicByaArea(@PathVariable("area")String area){
+        return ResponseEntity.ok(epidemicDataService.list(
+                Wrappers.<EpidemicDataEntity>lambdaQuery()
+                        .eq(EpidemicDataEntity::getArea, area)
+                ));
     }
 
 }
